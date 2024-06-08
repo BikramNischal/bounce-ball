1d9b1d8d104f147b5f5c3768b1fdd5c8dcadbfac
PsychicGeek <bikram.bh10@gmail.com>
2024-06-08 15:00:31 +0545

    code refactoring and optimization

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
-//get container and ball reference
-const ballContainer = document.querySelector("#ball-container");
-const ball = document.querySelector("#ball");
+// body styling
 const body = document.querySelector("body");
 body.style.backgroundColor = "#727273";
 body.style.color = "white";
 
-//container height and width
-const containerHeight = 500;
-const containerWidth = 500;
-
-// ball height and width
-const ballHeight = 40;
-const ballWidth = 40;
-
-//ball movement setup
-const speed = 5; //ball speed
-let y = 0; // ball position in y-axis
-let dy = 1; //change in distance
+//get ball container element
+const ballContainer = document.querySelector("#ball-container");
 
 //set container style
-ballContainer.style.height = `${containerHeight}px`;
-ballContainer.style.width = `${containerWidth}px`;
+ballContainer.style.height = `100vh`;
+ballContainer.style.width = `100%`;
 ballContainer.style.border = `1px solid #bdbebf`;
 ballContainer.style.position = `relative`;
 ballContainer.style.margin = `0px auto`;
 
-//set ball style
-ball.style.height = `${ballHeight}px`;
-ball.style.width = `${ballWidth}px`;
-ball.style.border = `1px solid #77a8f7`;
-ball.style.position = `absolute`;
-ball.style.borderRadius = "50%";
-ball.style.left = `${containerHeight / 2 - ballWidth}px`;
-ball.style.backgroundColor = "#77a8f7";
+class Ball {
+	constructor(posX = 0, posY = 0, color = "#77a8f7") {
+		//ball position height width and velocity
+		this.x = posX;
+		this.y = posY;
+		this.dx = 1;
+		this.dy = 1;
+		this.h = 40;
+		this.w = 40;
 
+		//ball styling
+		this.ball = document.createElement("div");
+		this.ball.style.height = `${this.h}px`;
+		this.ball.style.width = `${this.w}px`;
+		this.ball.style.border = `1px solid ${color}`;
+		this.ball.style.position = `absolute`;
+		this.ball.style.borderRadius = "50%";
+		this.ball.style.backgroundColor = color;
+		ballContainer.appendChild(this.ball);
+	}
 
-function boundryCollision(ballX = 0,ballY = 0){
-    //collsion detection for y-axis 
-    if(ballY >= containerHeight-ballHeight || ballY <= 0){
-        dy *= -1;
-    }
+	//draw the ball at x,y position
+	draw() {
+		this.ball.style.left = `${this.x}px`;
+		this.ball.style.top = `${this.y}px`;
+	}
 
-    
+	//move the ball by dx,dy
+	move(speed) {
+		this.x += this.dx * speed;
+		this.y += this.dy * speed;
+	}
 }
 
-setInterval(() => {
-	y += dy * speed;
-    boundryCollision(0 , y);
-	// ball.style.top = `${y}px`;
-}, 100);
+function boundryCollision(ball, ballContainerSize) {
+	//collsion detection for y-axis
+	if (ball.y >= ballContainerSize.height - ball.h || ball.y <= 0)
+		ball.dy *= -1;
+
+	if (ball.x >= ballContainerSize.width - ball.h || ball.x <= 0)
+		ball.dx *= -1;
+}
+
+const ball = new Ball(0, 0);
+function updateScreen() {
+	setInterval(() => {
+		//get client size of container
+		const ballContainerSize = ballContainer.getBoundingClientRect();
+		ball.move(5);
+		boundryCollision(ball, ballContainerSize);
+		ball.draw();
+	}, 10);
+}
+
+updateScreen();
