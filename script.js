2745b79ccefb1aed7c9427aa90412d01d1b85fda
PsychicGeek <bikram.bh10@gmail.com>
2024-06-09 16:43:24 +0545

    add ball collision

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ let ballContainerSize = ballContainer.getBoundingClientRect();
 // returns random x and y co-ordinate within container boundary
 function getRandom(containerHeight, containerWidth) {
 	const speed = Math.floor(Math.random() * 10);
-	const diameter = Math.floor(Math.random() * 40);
+	const diameter = Math.floor(Math.random() * 30);
 	const xaxis = Math.floor(Math.random() * containerWidth);
 	const yaxis = Math.floor(Math.random() * containerHeight);
 	return {
 		size: diameter < 20 ? 20 : diameter,
-		x: xaxis < diameter ? xaxis : xaxis - diameter,
-		y: yaxis < diameter ? yaxis : yaxis - diameter,
+		x: xaxis < 100 ? xaxis : xaxis - 500,
+		y: yaxis < 100 ? yaxis : yaxis - 500,
 		speed: speed ? speed : 1,
 		dx: Math.random() > 0.5 ? 1 : -1,
 		dy: Math.random() < 0.5 ? -1 : 1,
@@ -37,13 +37,12 @@ class Ball {
 	constructor(
 		posX = 0,
 		posY = 0,
-        size = 20,
+		size = 10,
 		speed = 5,
 		dx = 1,
 		dy = 1,
 		color = "#77a8f7"
 	) {
-		//ball position height width and velocity
 		this.x = posX;
 		this.y = posY;
 		this.dx = dx;
@@ -71,15 +70,41 @@ class Ball {
 
 	//move the ball by dx,dy
 	move() {
+		//collision detection for y-axis
+		if (this.y >= ballContainerSize.height - this.h || this.y <= 0)
+			this.dy *= -1;
+
+		//collision detection for x-axis
+		if (this.x >= ballContainerSize.width - this.h || this.x <= 0)
+			this.dx *= -1;
+
 		this.x += this.dx * this.speed;
 		this.y += this.dy * this.speed;
+		// this.centerX = this.x + Math.floor(this.w / 2);
+		// this.centerY = this.y + Math.floor(this.h / 2);
 	}
-}
 
-function displayBall(ball) {
-	ball.move();
-	boundaryCollision(ball);
-	ball.draw();
+	updateDirection() {
+		this.dx *= -1;
+		this.dy *= -1;
+	}
+
+	collision(otherBall) {
+			if (
+				this.x + this.w > otherBall.x &&
+				this.y + this.h > otherBall.y &&
+				otherBall.x + otherBall.w > this.x &&
+				otherBall.y + otherBall.h > this.y
+			) {
+                const tempdx = this.dx;
+                const tempdy = this.dy;
+                this.dx = otherBall.dx;
+                this.dy = otherBall.dy;
+
+                otherBall.dx = tempdx;
+                otherBall.dy = tempdy;
+			}
+	}
 }
 
 function generateBalls(num) {
@@ -93,7 +118,7 @@ function generateBalls(num) {
 			new Ball(
 				ballProp.x,
 				ballProp.y,
-                ballProp.size,
+				ballProp.size,
 				ballProp.speed,
 				ballProp.dx,
 				ballProp.dy
@@ -103,24 +128,22 @@ function generateBalls(num) {
 	return ballArr;
 }
 
-//rebounce on boundary collision
-function boundaryCollision(ball) {
-	//collsion detection for y-axis
-	if (ball.y >= ballContainerSize.height - ball.h || ball.y <= 0)
-		ball.dy *= -1;
-
-	if (ball.x >= ballContainerSize.width - ball.h || ball.x <= 0)
-		ball.dx *= -1;
-}
-
-const balls = generateBalls(10);
+const balls = generateBalls(500);
 
 function updateScreen() {
-	setInterval(() => {
-		//get client size of container
-		ballContainerSize = ballContainer.getBoundingClientRect();
-		balls.forEach(displayBall);
-	}, 10);
+    ballContainerSize = ballContainer.getBoundingClientRect();
+
+
+    //for ball collision
+	for (const ball of balls) {
+		ball.move();
+		ball.draw();
+		for (let j = balls.indexOf(ball) + 1; j < balls.length; ++j) {
+			ball.collision(balls[j]);
+		}
+	}
+
+	requestAnimationFrame(updateScreen);
 }
 
 // updateScreen();
